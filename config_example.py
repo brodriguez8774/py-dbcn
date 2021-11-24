e2dd8ab9b8b10e82302da82285b4c67e22434ecb
Brandon Rodriguez <brodriguez8774@gmail.com>
2021-11-23 19:06:17 -0500

    Create project config file

---
+++ config_example.py
@@ -0,0 +1,20 @@
+"""
+Example of project config file, for defining database connection settings.
+
+If modifying these values at all, please copy to a new "config.py" file and edit values there.
+"""
+
+# SqLite connection values.
+sqlite_config = {
+    'location': 'db_location'
+}
+
+
+# MySQL connection values.
+mysql_config = {
+    'host': '127.0.0.1',
+    'port': '3306',
+    'name': 'db_name',
+    'user': 'db_user',
+    'password': 'db_pass',
+}
