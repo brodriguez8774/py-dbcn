38627805976ecd0767ea824b15ee03bb3ca66a48
Brandon Rodriguez <brodriguez8774@gmail.com>
2021-11-23 20:00:48 -0500

    Create logic for a few initial database queries

--- src/connectors/core.py
+++ src/connectors/core.py
@@ -28,6 +28,7 @@ class AbstractDbConnector(ABC):
         self.database = self._get_related_database_class()
         self.display = self._get_related_display_class()
         self.query = self._get_related_query_class()
+        self.tables = self._get_related_tables_class()
         self.validate = self._get_related_validate_class()
 
     def __del__(self):
@@ -57,6 +58,12 @@ class AbstractDbConnector(ABC):
         """
         return BaseQuery(self)
 
+    def _get_related_tables_class(self):
+        """
+        Overridable method to get the related "tables functionality" class.
+        """
+        return BaseTables(self)
+
     def _get_related_validate_class(self):
         """
         Overridable method to get the related "validation functionality" class.
@@ -72,12 +79,49 @@ class BaseDatabase():
         logger.debug('Generating related (core) Database class.')
         self._base = parent
 
+    def _get(self, show=False):
+        """
+        Gets list of all currently-available databases.
+        """
+        # Generate and execute query.
+        query = 'SHOW DATABASES;'
+        results = self._base.query.execute(query)
+
+        # Convert to more friendly format.
+        formatted_results = []
+        for result in results:
+            formatted_results.append(result[0])
+        results = formatted_results
+
+        if show:
+            logger.info('results: {0}'.format(results))
+
+        # Return data.
+        return results
+
+    def show(self):
+        """
+        Displays all databases available for selection.
+        """
+        return self._get(show=True)
+
     def use(self, db_name):
         """
         Selects given database for use.
         """
+        # Get list of valid databases.
+        available_databases = self._get()
+
+        # Check if provided database matches value in list.
+        if db_name not in available_databases:
+            raise ValueError(
+                'Could not find database "{0}". Valid options are {1}.'.format(db_name, available_databases)
+            )
+
+        # Generate and execute query.
         query = 'USE {0};'.format(db_name)
         self._base.query.execute(query)
+        logger.info('Database changed to "{0}".'.format(db_name))
 
 
 class BaseDisplay():
@@ -99,7 +143,32 @@ class BaseQuery():
 
     def execute(self, query):
         """"""
-        logger.info(query)
+        logger.query(query)
+
+        # Create connection and execute query.
+        cursor = self._base.connection.cursor()
+        cursor.execute(query)
+
+        # Get results.
+        results = cursor.fetchall()
+
+        # Close connection.
+        self._base.connection.commit()
+        cursor.close()
+
+        # Return results.
+        if results is None:
+            results = []
+        return results
+
+
+class BaseTables():
+    """
+
+    """
+    def __init__(self, parent):
+        logger.debug('Generating related (core) Query class.')
+        self._base = parent
 
 
 class BaseValidate():
